5cccef1f9c0bfb77f78b41c62022dc77491a6f55
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2024-06-03 14:04:52 +0700

    js.js

--- sourceCode/assets/js/js.js
+++ sourceCode/assets/js/js.js
@@ -1,1 +1,43 @@
+var menuIcon = document.getElementsByClassName("menuIcon")[0];
 
+function displayNav() {
+    var nav = document.getElementsByTagName("nav")[0];
+    nav.classList.toggle('displayNav');
+}
+
+menuIcon.addEventListener("click", displayNav);
+
+var MobileLogin = document.getElementById("mobileFormLogin");
+
+function displayNav() {
+    var nav = document.getElementsByTagName("nav")[0];
+    nav.classList.toggle('displayNav');
+}
+
+MobileLogin.addEventListener("click", displayNav);
+
+var MobileSignin = document.getElementById("mobileFormSignin");
+
+function displayNav() {
+    var nav = document.getElementsByTagName("nav")[0];
+    nav.classList.toggle('displayNav');
+}
+
+MobileSignin.addEventListener("click", displayNav);
+
+var MobileRandomArticle = document.getElementById("swichMobileForm");
+function displayNav() {
+    var nav = document.getElementsByTagName("nav")[0];
+    nav.classList.toggle('displayNav');
+}
+
+MobileRandomArticle.addEventListener("click", displayNav);
+
+// var MobileSearchForm = document.getElementById("mobileSearchForm");
+
+function getSearchForm() {
+    var form = document.getElementsByClassName("form")[0];
+    form.classList.toggle('displaySearch');
+}
+
+// MobileSearchForm.addEventListener("click", getSearchForm);
