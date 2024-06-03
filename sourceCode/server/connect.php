13b9b054097ca1951d97160835bc903d63121d07
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2024-06-03 14:09:31 +0700

    connect.php

--- sourceCode/server/connect.php
+++ sourceCode/server/connect.php
@@ -1,1 +1,12 @@
+<?php
 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "article";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if(!$conn) {
+    die("connection failed: " . $conn->connect_error);
+}
